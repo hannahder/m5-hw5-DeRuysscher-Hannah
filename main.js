3b6a13afe56ef0d1cdb34433446391aa883c0e0e
Hannah De Ruysscher <hderuysscher@ufl.edu>
2021-02-11 07:57:27 -0500

    Added javascript

---
+++ main.js
@@ -0,0 +1,14 @@
+var pizza = {
+  crust: "thin",
+  cheese: "mozarella",
+  sauce: "light",
+  addToppings: console.log("Please add pepperoni and sausage")
+};
+
+var orderPizza = function() {
+  console.log(pizza.crust);
+  console.log(pizza.cheese);
+  console.log(pizza.sauce);
+  console.log(pizza.addToppings);
+}
+orderPizza();
